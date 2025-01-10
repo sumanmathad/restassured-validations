496d887baaffa56f5b0d7885b4b4acd3ec50fea7
suman.mathad <suman.mathad@oracle.com>
2025-01-10 17:01:01 +0530

    updates to football tests

--- src/test/java/restfundamentals/FootballTests.java
+++ src/test/java/restfundamentals/FootballTests.java
@@ -1,8 +1,19 @@
 package restfundamentals;
 
+import org.junit.jupiter.api.Test;
 import restfundamentals.config.FootBallConfig;
 
-public class FootballTests extends FootBallConfig {
+import static io.restassured.RestAssured.*;
 
+public class FootballTests extends FootBallConfig {
+    @Test
+    public void getDetailsOfOneArea(){
+
+        given()
+                .queryParam("areas",2076)
+        .when()
+                .get("/areas");
+
+    }
 
 }
